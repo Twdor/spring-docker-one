d3dbf5e6aab414b3600b970a78515b2ae219918f
Twdor <adrianghitescu0713@gmail.com>
2021-12-16 18:33:51 +0200

    Initial commit

---
+++ src/main/java/com/example/springdockerone/SpringDockerOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdockerone/SpringDockerOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
